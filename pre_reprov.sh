98b6e20609abb8dfdd095b627ac00008e786b757
aleksozolins <aleks@aleksozolins.com>
2020-03-23 16:03:38 -0400

    undo credential cacheing

--- pre_reprov.sh
+++ pre_reprov.sh
@@ -17,8 +17,8 @@ alias cfg='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 # eliminate recursion problems
 echo ".cfg" >> .gitignore
 
-# enable git credential cacheing for 2 minutes
-git config --global credential.helper 'cache --timeout 120'
+# enable git credential cacheing for 2 minutes (for some reason this doesn't work)
+# git config --global credential.helper 'cache --timeout 120'
 
 # clone the repo
 git clone --bare https://github.com/aleksozolins/dotfiles.git $HOME/.cfg
