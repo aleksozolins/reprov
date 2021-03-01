70acde260765c8f198e45d3383e811c58a2b6cbf
aleksozolins <aleks@aleksozolins.com>
2021-02-28 22:23:27 -0500

    fix cups enable command

--- reprov.sh
+++ reprov.sh
@@ -168,7 +168,7 @@ if [[ $printer == y* ]]
   sudo pacman -S --noconfirm cups system-config-printer ghostscript avahi nss-mdns
   sudo sed -i '10s/.*/hosts: files mymachines myhostname mdns_minimal [NOTFOUND=return] resolve [!UNAVAIL=return] dns/' /etc/nsswitch.conf
   sudo systemctl enable --now avahi-daemon.service
-  sudo systemctl enable --now org.cups.cupsd.service
+  sudo systemctl enable --now cups.service
   else
   echo "Onward!!!"
 fi
