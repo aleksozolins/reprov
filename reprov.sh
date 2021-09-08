f61474fe7a900862ef903fe8cfc5c99598a5810c
aleksozolins <aleks@aleksozolins.com>
2021-09-08 16:45:10 -0400

    small error

--- reprov.sh
+++ reprov.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 clear
 echo "This script will set up your public and private git repositories, populate your home directory with dotfiles, and configure various programs."
-echo "It should be run from ~/repos/reprov"
+echo "It should be run from ~/repos/reprov_new/"
 echo "Make sure to run as user with sudo privileges and be accurate typing your credentials. You will need to type your git and GPG passphrase multiple times."
 
 read -p "Press Enter to begin..."
